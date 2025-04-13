afc9b4f166a3c501b1312e3f0bd6cb331326e1ea
sudesh sudhii <144050211+sudeshsudhii@users.noreply.github.com>
2025-04-14 00:56:17 +0530

    Add files via upload

---
+++ components/apps/vscode.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function VsCode() {
+    return (
+        <iframe src="https://github1s.com/sudeshsudhii/College-Management-System-project/blob/main/frontend/src/pages/Homepage.js" frameBorder="0" title="VsCode" className="h-full w-full bg-ub-cool-grey"></iframe>
+        
+    )
+}
+
+export const displayVsCode = () => {
+    <VsCode> </VsCode>
+}
